57693764cf2d97731123aff551c21de271d63739
Nikita Titov <netitov@gmail.com>
2021-01-03 15:11:51 +0300

    feat: adding static files

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use('/', express.static('public'));
+
 app.listen(PORT, ()=>{
   console.log(`Application is running on port ${PORT}`)
 })
